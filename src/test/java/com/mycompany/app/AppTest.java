ce24ddfc83d5118f416310a90568ae20385c6e4a
Rahul917797 <101493883+Rahul917797@users.noreply.github.com>
2026-04-07 12:01:47 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -7,19 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
+public class AppTest {
+
     @Test
     public void testAppConstructor() {
         App app1 = new App();
         App app2 = new App();
+        // Check that both instances return the same message
         assertEquals(app1.getMessage(), app2.getMessage());
     }
 
     @Test
-    public void testAppMessage()
-    {
+    public void testAppMessage() {
         App app = new App();
+        // Check that getMessage returns "Hello World!"
         assertEquals("Hello World!", app.getMessage());
     }
 }
